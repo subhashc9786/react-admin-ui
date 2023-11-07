53497e1fb9d242a006fd1419f4aa0f12bf5ac22b
Subhash Chander <69232495+subhashc9786@users.noreply.github.com>
2023-11-07 16:13:17 +0530

    Update Footer.tsx

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import './footer.scss'
 const Footer = () => {
     return (
         <div className='footer'>
-            <span>Email:-  subhashc9786@gmail.com</span>
+            <span>Email:- subhashc9786@gmail.com</span>
             <span>@ subhash Admin Dashboard</span>
         </div>
     )
